61be730b8818702ba5497ca0809428c0ab52a281
jpmmueller <jpmmueller@gmail.com>
2022-03-16 18:23:49 +0100

    Funktioniert nicht schlecht

--- ball.js
+++ ball.js
@@ -3,9 +3,15 @@ class Ball {
         this.x = x;
         this.y = y;
         this.r = 16;
+        this.yspeed = 0;
     }
 
     show(){
         circle(this.x, this.y, this.r * 2);
+    }
+
+    update(){
+        this.y = this.y + this.yspeed;
+        this.yspeed = this.yspeed + 0.03;
     }
 }
--- sketch.js
+++ sketch.js
@@ -2,18 +2,22 @@ let ball1;
 let ball2;
 let Balls = [];
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 800);
 
 }
 
 function draw() {
   background(220);
 
-
-  for (let i = 0; i < Balls.length; i++){
-    Balls[i].show();
+  for (let ball of Balls){
+    ball.show();
+    ball.update();
   }
-  // console.log(Balls);
+  for (let i = Balls.length - 1;i >= 0; i--){
+    if (Balls[i].y > height){
+      Balls.splice(i,1);
+    }
+  }
 }
 function mousePressed(){
   Balls.push(new Ball(mouseX, mouseY));
